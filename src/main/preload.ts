5e906b137bc44c24dbadb057f3acd22fe1488277
syukoGit <syukodev@outlook.com>
2023-07-25 15:27:51 +0200

    init

---
+++ src/main/preload.ts
@@ -0,0 +1,25 @@
+import { contextBridge, ipcRenderer, IpcRendererEvent } from 'electron';
+
+const electronHandler = {
+  ipcRenderer: {
+    sendMessage(channel: string, ...args: unknown[]) {
+      ipcRenderer.send(channel, ...args);
+    },
+    on(channel: string, func: (...args: unknown[]) => void) {
+      const subscription = (_event: IpcRendererEvent, ...args: unknown[]) =>
+        func(...args);
+      ipcRenderer.on(channel, subscription);
+
+      return () => {
+        ipcRenderer.removeListener(channel, subscription);
+      };
+    },
+    once(channel: string, func: (...args: unknown[]) => void) {
+      ipcRenderer.once(channel, (_event, ...args) => func(...args));
+    },
+  },
+};
+
+contextBridge.exposeInMainWorld('electron', electronHandler);
+
+export type ElectronHandler = typeof electronHandler;
